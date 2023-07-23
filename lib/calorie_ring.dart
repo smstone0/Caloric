78ac3e15fed4a384d4b21136c40cb57c231886c0
Sophie Stone <smstone04@gmail.com>
2023-07-23 13:48:24 +0100

    Use CircularProgressIndicator in CalorieRing

--- lib/calorie_ring.dart
+++ lib/calorie_ring.dart
@@ -4,32 +4,31 @@ class CalorieRing extends StatelessWidget {
   const CalorieRing({super.key, required this.size});
 
   final double size;
-  final int calories = 0;
-  final int target = 2000;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(100),
-      ),
-      child: SizedBox(
-        width: size,
-        height: size,
-        child: Padding(
-          padding: const EdgeInsets.all(3.0),
-          child: Card(
-            color: Theme.of(context).colorScheme.primaryContainer,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100),
+    int calories = 50;
+    int target = 2000;
+
+    double value = calories / target;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            width: size,
+            height: size,
+            child: CircularProgressIndicator(
+              value: value,
+              backgroundColor: Colors.white,
+              color: Colors.green,
+              strokeWidth: 7,
             ),
-            child: Center(
-                child: Text("$calories/$target",
-                    style: const TextStyle(fontSize: 16.5))),
           ),
-        ),
+          Text("$calories/$target", style: const TextStyle(fontSize: 16.5)),
+        ],
       ),
     );
   }
