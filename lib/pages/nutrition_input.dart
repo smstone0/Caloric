e25120594aaea0fe0f9a7f116bbe71e2e32c0e58
Sophie Stone <psyss16@exmail.nottingham.ac.uk>
2024-06-28 19:10:35 +0100

    Display g or ml depending on whether food/drink is selected - Remove separate widget for food buttons

--- lib/pages/nutrition_input.dart
+++ lib/pages/nutrition_input.dart
@@ -23,36 +23,38 @@ class AddNutritionPage extends StatelessWidget {
   }
 }
 
-class InputCard extends StatelessWidget {
+class InputCard extends StatefulWidget {
   const InputCard({super.key, required this.settings});
 
   final Settings settings;
 
   @override
+  State<InputCard> createState() => _InputCardState();
+}
+
+class _InputCardState extends State<InputCard> {
+  bool foodClick = false;
+  bool drinkClick = false;
+  late NutType type;
+  String unit = "";
+
+  @override
   Widget build(BuildContext context) {
-    Nutrition newNutrition;
-    String energy;
-    if (settings.energy.unit == EnergyUnit.calories) {
-      energy = 'kcal';
-    } else {
-      energy = 'kJ';
-    }
-    return Padding(
-      padding: const EdgeInsets.only(left: 10),
+    final ThemeData theme = Theme.of(context);
+
+    return Form(
       child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text(
-                "Add item",
-                style: TextStyle(fontSize: 18),
-              ),
+              Text("Add item", style: theme.textTheme.titleMedium),
               IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.close))
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.close),
+              ),
             ],
           ),
           Padding(
@@ -81,9 +83,43 @@ class InputCard extends StatelessWidget {
               onSubmitted: (value) {},
             ),
           ),
-          const Padding(
+          Padding(
             padding: EdgeInsets.only(bottom: 20),
-            child: ButtonRow(),
+            child: Row(
+              children: [
+                CustomButton(
+                    widget: const Text("Food"),
+                    onPressed: () {
+                      setState(() {
+                        foodClick = true;
+                        drinkClick = false;
+                        type = NutType.food;
+                        unit = 'g';
+                      });
+                    },
+                    colour: foodClick == true
+                        ? Theme.of(context).primaryColor
+                        : Colors.white,
+                    height: 40,
+                    width: 85),
+                const SizedBox(width: 5),
+                CustomButton(
+                    widget: const Text("Drink"),
+                    onPressed: () {
+                      setState(() {
+                        drinkClick = true;
+                        foodClick = false;
+                        type = NutType.drink;
+                        unit = 'ml';
+                      });
+                    },
+                    colour: drinkClick == true
+                        ? Theme.of(context).primaryColor
+                        : Colors.white,
+                    height: 40,
+                    width: 85)
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 20),
@@ -97,7 +133,10 @@ class InputCard extends StatelessWidget {
                     cursorColor: Colors.black,
                     decoration: InputDecoration(
                       filled: true,
-                      suffixIcon: Text(energy),
+                      suffixIcon: Text(
+                          widget.settings.energy.unit == EnergyUnit.calories
+                              ? 'kcal'
+                              : 'kJ'),
                       suffixIconConstraints:
                           const BoxConstraints(minWidth: 0, minHeight: 0),
                       enabledBorder: UnderlineInputBorder(
@@ -133,7 +172,7 @@ class InputCard extends StatelessWidget {
                   decoration: InputDecoration(
                     filled: true,
                     //Unit
-                    suffixIcon: const NutInputDropdown(),
+                    suffixIcon: Text(unit),
                     enabledBorder: UnderlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                       borderSide: BorderSide(
@@ -156,125 +195,6 @@ class InputCard extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class NutInputDropdown extends StatefulWidget {
-  const NutInputDropdown({super.key});
-
-  @override
-  State<NutInputDropdown> createState() => _NutInputDropdownState();
-}
-
-class _NutInputDropdownState extends State<NutInputDropdown> {
-  String display = 'Choose unit';
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> list = ['g', 'ml', 'custom'];
-
-    return Card(
-      color: Colors.white,
-      elevation: 0,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-        child: SizedBox(
-          width: 110,
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton(
-              selectedItemBuilder: (BuildContext context) {
-                return list.map<Widget>((String item) {
-                  return Center(
-                    child: Text(
-                      display,
-                      style: const TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  );
-                }).toList();
-              },
-              items: list.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value,
-                        style: TextStyle(
-                            color: Theme.of(context)
-                                        .primaryColor
-                                        .computeLuminance() >=
-                                    0.5
-                                ? Colors.black
-                                : Colors.white)));
-              }).toList(),
-              value: list.first,
-              style: DefaultTextStyle.of(context).style,
-              dropdownColor: Theme.of(context).primaryColor,
-              onChanged: (String? value) {
-                setState(() {});
-                display = value!;
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonRow extends StatefulWidget {
-  const ButtonRow({
-    super.key,
-  });
-
-  @override
-  State<ButtonRow> createState() => _ButtonRowState();
-}
-
-class _ButtonRowState extends State<ButtonRow> {
-  late NutType type;
-  bool foodClick = false;
-  bool drinkClick = false;
-
-  Color clickColour(bool click) {
-    if (click) {
-      return Theme.of(context).primaryColor;
-    }
-    return Colors.white;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Color foodColour = clickColour(foodClick);
-    Color drinkColour = clickColour(drinkClick);
-    return Row(
-      children: [
-        CustomButton(
-            widget: const Text("Food"),
-            onPressed: () {
-              setState(() {
-                foodClick = true;
-                drinkClick = false;
-                type = NutType.food;
-              });
-            },
-            colour: foodColour,
-            height: 40,
-            width: 85),
-        const SizedBox(width: 5),
-        CustomButton(
-            widget: const Text("Drink"),
-            onPressed: () {
-              setState(() {
-                drinkClick = true;
-                foodClick = false;
-                type = NutType.drink;
-              });
-            },
-            colour: drinkColour,
-            height: 40,
-            width: 85)
-      ],
     );
   }
 }
