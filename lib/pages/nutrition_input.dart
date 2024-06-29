5b9b46d2d0b83d3a62627ce963a4460777437f5c
Sophie Stone <psyss16@exmail.nottingham.ac.uk>
2024-06-29 20:02:04 +0100

    Adapt nutrition input page for editing existing entries - Allow multiple entries in database via  id

--- lib/pages/nutrition_input.dart
+++ lib/pages/nutrition_input.dart
@@ -5,9 +5,12 @@ import 'package:caloric/widgets/grey_card.dart';
 import 'package:flutter/material.dart';
 
 class AddNutritionPage extends StatelessWidget {
-  const AddNutritionPage({super.key, required this.settings});
+  const AddNutritionPage(
+      {super.key, required this.settings, this.nutrition, required this.id});
 
   final Settings settings;
+  final nutrition;
+  final int id;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +19,7 @@ class AddNutritionPage extends StatelessWidget {
       children: [
         const SizedBox(height: 20),
         GreyCard(
-          child: InputCard(settings: settings),
+          child: InputCard(settings: settings, nutrition: nutrition, id: id),
         ),
       ],
     ));
@@ -24,15 +27,36 @@ class AddNutritionPage extends StatelessWidget {
 }
 
 class InputCard extends StatefulWidget {
-  const InputCard({super.key, required this.settings});
+  const InputCard(
+      {super.key,
+      required this.settings,
+      required this.nutrition,
+      required this.id});
 
   final Settings settings;
+  final nutrition;
+  final int id;
 
   @override
   State<InputCard> createState() => _InputCardState();
 }
 
 class _InputCardState extends State<InputCard> {
+  @override
+  void initState() {
+    super.initState();
+    if (!(widget.nutrition == null)) {
+      if (widget.nutrition.type == NutType.food) {
+        foodClick = true;
+        unit = 'g';
+      }
+      if (widget.nutrition.type == NutType.drink) {
+        drinkClick = true;
+        unit = 'ml';
+      }
+    }
+  }
+
   bool foodClick = false;
   bool drinkClick = false;
   late NutType type;
@@ -52,7 +76,8 @@ class _InputCardState extends State<InputCard> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Add item", style: theme.textTheme.titleMedium),
+              Text(widget.nutrition == null ? "Add item" : "Edit item",
+                  style: theme.textTheme.titleMedium),
               IconButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -64,6 +89,7 @@ class _InputCardState extends State<InputCard> {
           Padding(
             padding: const EdgeInsets.only(top: 20, bottom: 20, right: 50),
             child: TextFormField(
+              initialValue: widget.nutrition?.item,
               cursorColor: Colors.black,
               decoration: InputDecoration(
                 filled: true,
@@ -140,6 +166,7 @@ class _InputCardState extends State<InputCard> {
                   width: 90,
                   height: 30,
                   child: TextFormField(
+                    initialValue: widget.nutrition?.energy.toString(),
                     cursorColor: Colors.black,
                     decoration: InputDecoration(
                       filled: true,
@@ -185,6 +212,7 @@ class _InputCardState extends State<InputCard> {
                 width: 195,
                 height: 30,
                 child: TextFormField(
+                  initialValue: widget.nutrition?.quantity.toString(),
                   cursorColor: Colors.black,
                   decoration: InputDecoration(
                     filled: true,
@@ -218,13 +246,13 @@ class _InputCardState extends State<InputCard> {
             ],
           ),
           ElevatedButton(
+              //TODO: Update main nutrition page
               onPressed: () {
                 if (_formKey.currentState!.validate() && foodClick ||
                     drinkClick) {
                   DateTime time = DateTime.now();
                   NutritionDatabase().insertNutrition(Nutrition(
-                      //TODO: id = last + 1
-                      id: 0,
+                      id: widget.id,
                       item: itemName,
                       energy: energy,
                       type: type,
@@ -233,7 +261,9 @@ class _InputCardState extends State<InputCard> {
                       creationDate: '${time.day}/${time.month}/${time.year}'));
                 }
               },
-              child: const Text("Add"))
+              child: widget.nutrition == null
+                  ? const Text("Add")
+                  : const Text("Edit"))
         ],
       ),
     );
