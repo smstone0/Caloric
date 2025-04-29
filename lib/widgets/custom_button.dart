196500a70a7a80031ad5f553fa7f185250a833e6
smstone0 <smstone04@gmail.com>
2025-04-29 20:27:47 +0100

    Simplify CustomButton - Remove hardcoded widths and heights - Remove size enum

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,41 +1,16 @@
 import 'package:flutter/material.dart';
 
-enum Size { small, medium, large }
-
-double getHeight(Size size) {
-  switch (size) {
-    case Size.small:
-      return 40;
-    case Size.medium:
-      return 50;
-    default:
-      return 38;
-  }
-}
-
-double getWidth(Size size) {
-  switch (size) {
-    case Size.small:
-      return 85;
-    case Size.medium:
-      return 105;
-    default:
-      return 190;
-  }
-}
-
 class CustomButton extends StatelessWidget {
-  const CustomButton(
-      {super.key,
-      required this.widget,
-      required this.onPressed,
-      required this.colour,
-      required this.size});
+  const CustomButton({
+    super.key,
+    required this.widget,
+    required this.onPressed,
+    required this.colour,
+  });
 
   final Text widget;
   final VoidCallback onPressed;
   final Color colour;
-  final Size size;
 
   @override
   Widget build(BuildContext context) {
@@ -43,8 +18,6 @@ class CustomButton extends StatelessWidget {
         style: const TextStyle(
             color: Colors.black, fontWeight: FontWeight.normal));
     return SizedBox(
-      height: getHeight(size),
-      width: getWidth(size),
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
